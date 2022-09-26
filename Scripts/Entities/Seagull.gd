22d3249a9bb86c460b606eca1489e976ca371a0c
Lev <lev.gluzdov@gmail.com>
2022-09-26 16:15:14 -0400

    fix fry stealing detection

--- Scripts/Entities/Seagull.gd
+++ Scripts/Entities/Seagull.gd
@@ -4,15 +4,13 @@ var has_fry: bool = false
 onready var sprite = $AnimatedSprite
 
 func _ready():
-	$VisibilityNotifier2D.connect("screen_entered", self, "_on_VisibilityNotifier2D_screen_exited")
+	$VisibilityNotifier2D.connect("screen_exited", self, "_on_VisibilityNotifier2D_screen_exited")
 
 func _on_VisibilityNotifier2D_screen_exited():
-	print("Triggered")
-#	if has_fry:
-#		print("player damaged")
-#	die()
+	if has_fry:
+		print("player damaged")
+	die()
 
 func die():
-	$VisibilityNotifier2D.disconnect("screen_entered", self, "_on_VisibilityNotifier2D_screen_exited")
-	print(str(self.is_connected("screen_entered", self, "_on_VisibilityNotifier2D_screen_exited")) + " Connected")
+	$VisibilityNotifier2D.disconnect("screen_exited", self, "_on_VisibilityNotifier2D_screen_exited")
 	.die()
